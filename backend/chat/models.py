34acd7e63bb14761894d05387f76af0a54d7bd59
김병민 <gimbyeongmin@byeong.local>
2024-11-20 04:11:40 +0900

    chat models작성 및 기본설정

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,8 +1,22 @@
 from django.db import models
+from accounts.models import *
+
 
 # Create your models here.
 
 
 class ChatRoom(models.Model):
-    name = models.CharField(max_length=50)
-    create_time = models.DateTimeField(auto_now_add=True)    
+    name = models.CharField(max_length=50,unique=True)
+    users = models.ManyToManyField(User, related_name="chat_rooms")
+    add_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Message(models.Model):
+    chat_room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE, related_name="messages")
+    sender_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(blank=True)
+    image = models.FileField(upload_to="chat_image",blank=True, null=True)
+    add_date = models.DateTimeField(auto_now_add=True)
